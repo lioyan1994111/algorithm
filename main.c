7af6878517af32e76e1b128040c1aaf08c063771
liy <1064012668@qq.com>
2021-02-05 09:36:25 +0800

    构建1208

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include "simple/Title643.c"
+#include "medium//Title1208.c"
 
 
 int main() {
 
     int a[5] = {0,4,0,3,2};
-    double avn = findMaxAverage(a, 5, 1);
+    double avn = equalSubstring(a, 5, 1);
     printf("%f",avn);
 
     return 0;
